20bd4c37043e69c83156bcc619a26d24e20f5c9e
Wojtek <wojteklag@gmail.com>
2023-02-04 00:52:42 +0000

    set up friends table view and boiler plate code

--- Walking Buddy/FriendsVC.swift
+++ Walking Buddy/FriendsVC.swift
@@ -12,10 +12,31 @@ class FriendsVC: UIViewController {
 	
 	//Reference to db manager
 	private let db = DBManager.shared
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+	
+	//Stores records with profiles of user's friends
+	private var friendsArray: [CKRecord] = []
+	
+	//Controls refreshing of table view
+	private let refreshControl = UIRefreshControl()
+	
+	//Shows a list of user's friends
+	@IBOutlet var tableView: UITableView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		//Set up table view
+		tableView.delegate = self
+		tableView.dataSource = self
+		
+		//Set up refresh control
+		tableView.refreshControl = refreshControl
+		tableView.backgroundView = refreshControl
+		refreshControl.addTarget(self, action: #selector(refreshTable(_:)), for: .valueChanged)
+		refreshControl.tintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+		
+		fetchData()
+	}
 	
 	//When "+" button is tapped
 	@IBAction func addTapped(_ sender: Any) {
@@ -26,6 +47,16 @@ class FriendsVC: UIViewController {
 	@IBAction func myProfile(_ sender: Any) {
 		AppDelegate.get().setDesiredTabIndex(4)
 		showVC(identifier: "tabController")
+	}
+	
+	//Gets user's friends data from db and adds to friendsArray. Reloads table view.
+	private func fetchData() {
+		
+	}
+	
+	//Objective-C function to refresh the table view. Used for refreshControl.
+	@objc private func refreshTable(_ sender: Any) {
+		fetchData()
 	}
 	
 	//Shows view controller with given identifier
@@ -48,5 +79,36 @@ class FriendsVC: UIViewController {
 		alert.addAction(UIAlertAction(title: "OK", style: .default))
 		self.present(alert, animated: true)
 	}
+}
+	
+//Table view setup
 
+extension FriendsVC: UITableViewDelegate {
+	//When row in table is tapped
+	public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+}
+
+extension FriendsVC: UITableViewDataSource {
+	//Returns the number of rows for the table
+	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return friendsArray.count
+	}
+	
+	//Creates and returns a cell
+	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		//Create cell from reusable cell
+		let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
+		//cell.textLabel?.text =
+		//cell.textLabel?.textColor = UIColor.black
+		
+		//Set selection highlight colour
+		let bgColourView = UIView()
+		bgColourView.backgroundColor = UIColor.darkGray
+		cell.selectedBackgroundView = bgColourView
+		
+		return cell
+	}
 }
