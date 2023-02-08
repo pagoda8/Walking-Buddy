d9bd2f73041e49b9b9359a13a0fe142dee569635
Wojtek <wojteklag@gmail.com>
2023-02-08 21:56:12 +0000

    changed favourite tab to challenges tab

---
+++ Walking Buddy/Main 5 Tabs/ChallengesVC.swift
@@ -0,0 +1,16 @@
+//
+//  ChallengesVC.swift
+//  Walking Buddy
+//
+//  Created by Wojtek on 08/02/2023.
+//
+
+import UIKit
+
+class ChallengesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
